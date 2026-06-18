01b81c7681f184737e1f916ebb511d0b04c2ec0b
EllyChun <chun1000mk@gmail.com>
2026-06-18 21:07:21 +0900

    added favicon

--- scripts/prepare-cloudflare.js
+++ scripts/prepare-cloudflare.js
@@ -6,6 +6,7 @@ await rm(outputDir, { recursive: true, force: true });
 await mkdir(new URL("./src/", outputDir), { recursive: true });
 
 await cp(new URL("../index.html", import.meta.url), new URL("./index.html", outputDir));
+await cp(new URL("../favicon.ico", import.meta.url), new URL("./favicon.ico", outputDir));
 await cp(new URL("../src/tarot.js", import.meta.url), new URL("./src/tarot.js", outputDir));
 await cp(new URL("../decks/", import.meta.url), new URL("./decks/", outputDir), {
   recursive: true
